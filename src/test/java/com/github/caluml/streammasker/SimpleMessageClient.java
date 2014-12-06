820f76754d487e0a75d1b98f4b8f7266a7d636d5
Calum <calum@home>
2014-12-06 20:51:47 +0000

    Add simple CLI message servers to demonstrate

---
+++ src/test/java/com/github/caluml/streammasker/SimpleMessageClient.java
@@ -0,0 +1,35 @@
+package com.github.caluml.streammasker;
+
+import java.io.*;
+import java.net.Socket;
+import java.security.SecureRandom;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Simple message client. Reads from stdin, and writes to the MaskingOutputStream.
+ */
+public class SimpleMessageClient {
+
+    public static void main(String args[]) throws Exception {
+
+        final Socket socket = new Socket("127.0.0.1", 7733);
+        System.out.println("Client: Opened socket to " + socket.getRemoteSocketAddress());
+        final OutputStream outputStream = socket.getOutputStream();
+
+        final MaskingOutputStream maskingOutputStream = new MaskingOutputStream(outputStream, 8, new SecureRandom(), 1000, TimeUnit.MILLISECONDS);
+        new Thread(maskingOutputStream).start();
+
+        final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(maskingOutputStream));
+
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Client: Waiting for input");
+        String line;
+        do {
+            line = bufferedReader.readLine();
+            bufferedWriter.write(line + "\n");
+            bufferedWriter.flush();
+            System.out.println("Client: Wrote: " + line);
+        } while (line != null);
+    }
+
+}
---
+++ src/test/java/com/github/caluml/streammasker/SimpleMessageServer.java
@@ -0,0 +1,35 @@
+package com.github.caluml.streammasker;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Simple message server. Wraps a standard network socket in the MaskingInputStream, and reads lines from that.
+ */
+public class SimpleMessageServer {
+
+    public static void main(String args[]) throws Exception {
+
+        final ServerSocket serverSocket = new ServerSocket(7733);
+        System.out.println("Server: Listening on 7733");
+        final Socket socket = serverSocket.accept();
+        System.out.println("Server: Accepted connection from " + socket.getRemoteSocketAddress());
+        final InputStream inputStream = socket.getInputStream();
+
+        final MaskingInputStream maskingInputStream = new MaskingInputStream(inputStream, 8);
+        new Thread(maskingInputStream).start();
+
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(maskingInputStream));
+        String line;
+        do {
+            line = bufferedReader.readLine();
+            System.out.println("Server: Received " + line.length() + " bytes: " + line);
+        } while (line != null);
+
+        System.out.println("Server: Exiting");
+    }
+
+}
